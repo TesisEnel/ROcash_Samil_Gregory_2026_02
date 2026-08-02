f8ff3be1ce30bcb1346a9693318de7fefae2a58f
Sa1kxd <samildelacruz1@gmail.com>
2026-08-02 19:33:16 -0400

    feat ✨: Creando Mappers

---
+++ app/src/main/java/ucne/edu/rocash/data/mapper/Mappers.kt
@@ -0,0 +1,42 @@
+package ucne.edu.rocash.data.mapper
+
+import ucne.edu.rocash.data.local.entity.EstacionVentasEntity
+import ucne.edu.rocash.data.local.entity.HojaRutaEntity
+import ucne.edu.rocash.data.local.entity.RegistroRecoleccionEntity
+import ucne.edu.rocash.domain.model.EstacionVentas
+import ucne.edu.rocash.domain.model.EstadoRuta
+import ucne.edu.rocash.domain.model.HojaRuta
+import ucne.edu.rocash.domain.model.RegistroRecoleccion
+
+fun HojaRutaEntity.toDomain(): HojaRuta {
+    return HojaRuta(
+        id = this.id,
+        recolectorId = this.recolectorId,
+        fechaCreacion = this.fechaCreacion,
+        estado = EstadoRuta.valueOf(this.estado)
+    )
+}
+
+fun EstacionVentasEntity.toDomain(): EstacionVentas {
+    return EstacionVentas(
+        id = this.id,
+        nombre = this.nombre,
+        direccion = this.direccion,
+        agenteId = this.agenteId
+    )
+}
+
+fun RegistroRecoleccion.toEntity(): RegistroRecoleccionEntity {
+    return RegistroRecoleccionEntity(
+        id = this.id,
+        hojaRutaId = this.hojaRutaId,
+        estacionId = this.estacionId,
+        ventaBruta = this.ventaBruta,
+        porcentajeCliente = this.porcentajeCliente,
+        montoEsperado = this.montoEsperado,
+        montoRecolectado = this.montoRecolectado,
+        montoDeuda = this.montoDeuda,
+        estadoVisita = this.estadoVisita.name,
+        notaIncidencia = this.notaIncidencia
+    )
+}
