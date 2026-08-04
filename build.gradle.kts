525beb341b7a5236e521b0ddbe615ae88c1f29b4
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-08-03 21:38:28 -0400

    Fix Resuelve bug relacionado con las versiones de Ksp y Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.dagger.hilt.android") version "2.60" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
+    id("com.google.dagger.hilt.android") version "2.60.1" apply false
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
 }
