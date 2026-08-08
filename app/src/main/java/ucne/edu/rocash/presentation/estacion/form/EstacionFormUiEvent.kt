e9d080a8ff5259951e52e1aa50f7015012f1231d
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-08-08 13:50:17 -0400

    Actualiza EstacionFormUiEvent.kt y añade los eventos OnAgenteSeleccionado y ResetSuccessState

--- app/src/main/java/ucne/edu/rocash/presentation/estacion/form/EstacionFormUiEvent.kt
+++ app/src/main/java/ucne/edu/rocash/presentation/estacion/form/EstacionFormUiEvent.kt
@@ -1,8 +1,9 @@
 package ucne.edu.rocash.presentation.estacion.form
 
-sealed class EstacionFormUiEvent {
-    data class OnNombreChange(val value: String) : EstacionFormUiEvent()
-    data class OnDireccionChange(val value: String) : EstacionFormUiEvent()
-    data class OnAgenteIdChange(val value: String) : EstacionFormUiEvent()
-    object GuardarEstacion : EstacionFormUiEvent()
+sealed interface EstacionFormUiEvent {
+    data class OnNombreChange(val value: String) : EstacionFormUiEvent
+    data class OnDireccionChange(val value: String) : EstacionFormUiEvent
+    data class OnAgenteSeleccionado(val id: String, val nombre: String) : EstacionFormUiEvent
+    object GuardarEstacion : EstacionFormUiEvent
+    object ResetSuccessState : EstacionFormUiEvent
 }
