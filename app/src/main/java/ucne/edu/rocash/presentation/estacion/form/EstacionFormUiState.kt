7d516b88a3bd89db621542d1e356bb4309fb80c5
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-08-08 13:49:20 -0400

    Actualiza EstacionFormUiState.kt y añade los estados AgenteNombreSeleccionado y agentesDisponibles

--- app/src/main/java/ucne/edu/rocash/presentation/estacion/form/EstacionFormUiState.kt
+++ app/src/main/java/ucne/edu/rocash/presentation/estacion/form/EstacionFormUiState.kt
@@ -1,9 +1,13 @@
 package ucne.edu.rocash.presentation.estacion.form
 
+import ucne.edu.rocash.domain.agenteVentas.model.AgenteVentas
+
 data class EstacionFormUiState(
     val nombre: String = "",
     val direccion: String = "",
     val agenteId: String = "",
+    val agenteNombreSeleccionado: String = "",
+    val agentesDisponibles: List<AgenteVentas> = emptyList(),
     val isLoading: Boolean = false,
     val isSuccess: Boolean = false,
     val errorMessage: String? = null
