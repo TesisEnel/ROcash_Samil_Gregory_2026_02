d7629eade2e887b681c126a1539c14f1bb7c9cb9
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-08-09 08:41:34 -0400

    Cambia modelo HojaRuta

--- app/src/main/java/ucne/edu/rocash/domain/hojaRuta/model/HojaRuta.kt
+++ app/src/main/java/ucne/edu/rocash/domain/hojaRuta/model/HojaRuta.kt
@@ -1,14 +1,17 @@
-package ucne.edu.rocash.domain.model
+package ucne.edu.rocash.domain.hojaRuta.model
+
+import ucne.edu.rocash.domain.estacion.model.EstacionVentas
 
 enum class EstadoRuta { PENDIENTE, EN_PROGRESO, CERRADA }
 
 data class HojaRuta(
-    val id: String,
+    val id: Int = 0,
     val recolectorId: String,
     val fechaCreacion: Long = System.currentTimeMillis(),
     val estado: EstadoRuta,
     val totalVentaBruta: Double = 0.0,
     val totalPorcentajeClientes: Double = 0.0,
     val totalRecaudado: Double = 0.0,
-    val totalDeudas: Double = 0.0
+    val totalDeudas: Double = 0.0,
+    val estaciones: List<EstacionVentas> = emptyList()
 )
