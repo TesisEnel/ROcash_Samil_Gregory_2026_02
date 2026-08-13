6ae5bc05b8d263793979e3cc91c83fa59117a68a
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-08-12 21:53:17 -0400

    Refactoriza todo el paquete agenteVentas.list

--- app/src/main/java/ucne/edu/rocash/presentation/agenteVentas/list/AgenteLisUiState.kt
+++ app/src/main/java/ucne/edu/rocash/presentation/agenteVentas/list/AgenteLisUiState.kt
@@ -5,6 +5,9 @@ import ucne.edu.rocash.domain.agenteVentas.model.AgenteVentas
 data class AgenteListUiState(
     val isLoading: Boolean = false,
     val agentes: List<AgenteVentas> = emptyList(),
-    val searchQuery: String = "",
-    val errorMessage: String? = null
+    val message: String? = null,
+    val navigateToCreate: Boolean = false,
+    val navigateToEditId: Int? = null,
+    val error: String? = null,
+    val searchQuery: String = ""
 )
--- app/src/main/java/ucne/edu/rocash/presentation/agenteVentas/list/AgenteListUiEvent.kt
+++ app/src/main/java/ucne/edu/rocash/presentation/agenteVentas/list/AgenteListUiEvent.kt
@@ -3,6 +3,13 @@ package ucne.edu.rocash.presentation.agenteVentas.list
 import ucne.edu.rocash.domain.agenteVentas.model.AgenteVentas
 
 sealed interface AgenteListUiEvent {
-    data class OnSearchQueryChange(val query: String) : AgenteListUiEvent
-    data class ToggleEstadoAgente(val agente: AgenteVentas) : AgenteListUiEvent
+    data object Load : AgenteListUiEvent
+    data object Refresh : AgenteListUiEvent
+    data class Delete(val id: Int) : AgenteListUiEvent
+    data class ShowMessage(val message: String) : AgenteListUiEvent
+    data object ClearMessage : AgenteListUiEvent
+    data object CreateNew : AgenteListUiEvent
+    data class Edit(val id: Int) : AgenteListUiEvent
+    data class SearchQueryChanged(val query: String) : AgenteListUiEvent
+    data class ToggleEstado(val agente: AgenteVentas) : AgenteListUiEvent
 }
